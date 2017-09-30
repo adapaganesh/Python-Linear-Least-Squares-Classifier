04f7402d9aa476d5596b6711f8b2967c2afe778c
ganesh <adapaganesh@gmail.com>
2017-09-30 13:04:33 +0530

    Incremental change: debug statements added

--- LLS.py
+++ LLS.py
@@ -15,26 +15,26 @@ def train(x,y):
 	D = x.shape[1] + 1
 	# K = Number of classes
 	K = y.shape[1]
-	
+
 	# Build the sums of xi*xi' and xi*yi'
 	sum1 = np.zeros((D,D)) # init placeholder
 	sum2 = np.zeros((D,K))
 	i = 0
 	for x_i in x:						# loop over all vectors
-		x_i = np.append(1, x_i) 		# augment vector with a 1 
-		y_i = y[i]						
+		x_i = np.append(1, x_i) 		# augment vector with a 1
+		y_i = y[i]
 		sum1 += np.outer(x_i, x_i)		# find xi*xi'
 		sum2 += np.outer(x_i, y_i)		# find xi*yi'
 		i += 1
-	
+
 	# Check that condition number is finite
 	# and therefore sum1 is nonsingular (invertable)
 	while matrix_rank(sum1) != D:
 		# Naive choice of sigma.
 		# Could cause inaccuracies when sum1 has small values
 		# However, in most cases the matrix WILL be invertable
-		sum1 = sum1 + 0.001 * np.eye(D) 
-	
+		sum1 = sum1 + 0.001 * np.eye(D)
+
 	# Return weight vector
 	# Weight vector multiplies sums and inverse of sum1
 	return np.dot(inv(sum1),sum2)
@@ -51,7 +51,7 @@ def predict(W, x):
 
 	# Solve W'*x
 	values = list(np.dot(W.T,x))
-	
+
 	# Find maxima of values
 	winners = [i for i, x in enumerate(values) if x == max(values)] # indexes of maxima
 	# Flip a coin to decide winner
@@ -76,7 +76,7 @@ def fixLabels(y):
 		# Each list is the size of the largest class number
 		size = max(y)
 		temp = [0 for j in range(size + 1)]	# initalize list with zeroes
-		temp[y[i]] = 1		
+		temp[y[i]] = 1
 		newY.append(temp)	# add to matrix
 	return np.matrix(newY)
 
@@ -91,10 +91,12 @@ def test(a,b, split):
 
 	# Build weight vector from training data
 	W = train(a[:split],b[:split])
+	print 'W'
+	print W
 	# Build test sets
 	x = a[split:]
 	y = b[split:]
-	
+
 	total = y.shape[0]
 	i = 0
 	hits = 0
@@ -120,7 +122,7 @@ def main():
 	head = False
 	if "--head" in sys.argv:
 		head = True
-	
+
 	data = []
 	classes = []
 	f = open(sys.argv[1]) # open data file
@@ -139,38 +141,40 @@ def main():
 				data.append(map(lambda x: float(x), line[:-1]))
 				# Add class to list
 				classes.append(line[-1])
-			
+
 	finally:
 		f.close()
-	
+
 	# Convert class names to a number
 	classes = map(lambda x: list(set(classes)).index(x), classes)
-	
+
 	# Final preperations for attributes and classes
 	x = np.matrix(data)
 	y = fixLabels(classes)
 
-
 	# shuffle data for better accuracy
 	z = [] # temp array
 	size = x.shape[0] - 1
+
 	for i in range(size):
 		z.append((x[i],y[i]))
 	shuffle(z)
+
 	for i in range(size):
 		x[i] = z[i][0]
 		y[i] = z[i][1]
-		
+
 	# scale data so it fits in range (0,1)
 	for i in range(size):
 		x[i] = x[i] / x.max()
-	
+
 	# train and test data
-	print "50% Train/50% Test"
+	print "100% Train/0% Test"
 	split = int(size * 0.5)
 	test(x,y,split)
 	print "\n"
 
+'''
 	print "40% Train/60% Test"
 	split = int(size * 0.4)
 	test(x,y,split)
@@ -189,6 +193,7 @@ def main():
 	print "10% Train/90% Test"
 	split = int(size * 0.1)
 	test(x,y,split)
+'''
 
 # Python doesnt call main() by default
 if __name__ == "__main__":
